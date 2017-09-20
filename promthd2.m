2f6671dee718b64382e3ac33c89e5d880e48cbc0
shinnosuke ishida <sky.ciliegio@gmail.com>
2017-09-21 05:10:59 +0900

    オープニング処理

--- promthd2.m
+++ promthd2.m
@@ -3,8 +3,8 @@ num=50;%‰æ‘œ‚Ì—Ê‚Å•ÏX
 imcell2=cell(1,num);
 t_cell=cell(1,num);
 j_cell=cell(1,num);
-% se_bo  = strel('disk', 1); %–c’£—p (disk:‰~”Õó‚Ì”¼Œa5)
-% se_shu = strel('disk', 1); %ûk—p (disk:‰~”Õó‚Ì”¼Œa5)
+se_bo  = strel('disk', 3); %–c’£—p (disk:‰~”Õó‚Ì”¼Œa)
+se_shu = strel('disk', 3); %ûk—p (disk:‰~”Õó‚Ì”¼Œa)
 
 %‰æ‘œ‚Ì“Ç‚İ‚İ(˜A”Ôƒtƒ@ƒCƒ‹‚É‚·‚é)
 for i=1:num
@@ -17,7 +17,7 @@ end
 omega = 0.8;
 win_size = 6;
 r = 15;
-res = 0.001;
+res = 0.00001;
 A=make_A(imcell2{1,1}, win_size);
 trans_est = get_transmission_estimate(imcell2{1,1}, A, omega, win_size);
 t = guided_filter(rgb2gray(imcell2{1,1}), trans_est, r, res);
@@ -28,14 +28,12 @@ j_cell{1,1} = get_radiance(imcell2{1,2}, t_cell{1,1}, A);
 for i=2:num
    
    t_cell{1,i}=make_t1(t_cell{1,i-1},j_cell{1,i-1},imcell2{1,i-1},imcell2{1,i},A);
-   %ƒƒfƒBƒAƒ“ƒtƒBƒ‹ƒ^ver
-   %t_cell{1,i}=medfilt2(make_t1(t_cell{1,i-1},j_cell{1,i-1},imcell2{1,i-1},imcell2{1,i},A));
-   %GIFilter.ver
-   %t_cell{1,i} = guided_filter(rgb2gray(imcell2{1,i}), t_cell{1,i}, r, res);
-   
-%    t_cell{1,i} = imerode(t_cell{1,i}, se_shu); %ûk(imerode) 
-%    t_cell{1,i} = imerode(t_cell{1,i}, se_bo); %–c’£(imdilate)
-   
+   %GIFilter&opening.ver
+   if i==10||20||30||40||50
+       t_cell{1,i} = imerode(t_cell{1,i}, se_shu); %ûk(imerode)
+       t_cell{1,i} = imerode(t_cell{1,i}, se_bo); %–c’£(imdilate)
+   end
+   t_cell{1,i} = guided_filter(rgb2gray(imcell2{1,i}), t_cell{1,i}, r, res);   
    j_cell{1,i} = get_radiance(imcell2{1,i}, t_cell{1,i}, A);
    
    imgname=strcat('result\J\J',num2str(i),'.bmp');
